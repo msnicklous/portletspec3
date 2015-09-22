97f85cb8264d3e41d3762e000960359c76667784
Scott Nicklous <Scott.Nicklous@de.ibm.com>
2015-09-22 13:02:06 +0200

    Removed the ResourceRequest#getPageState() method and replaced it by the PAGE_STATE request attribute name constant.

--- portlet-api_3.0/src/main/java/javax/portlet/filter/ResourceRequestWrapper.java
+++ portlet-api_3.0/src/main/java/javax/portlet/filter/ResourceRequestWrapper.java
@@ -186,12 +186,4 @@ public class ResourceRequestWrapper extends PortletRequestWrapper implements Res
       return request.getResourceParameters();
    }
 
-   /* (non-Javadoc)
-    * @see javax.portlet.ResourceRequest#getPageState()
-    */
-   public String getPageState() {
-      return request.getPageState();
-   }
-
-
 }
